aa67be53f7cdff1b2d5ac932c29d6555c6f88065
fourbarman <maks.java@yandex.ru>
2021-12-14 10:37:34 +0400

    Thread. Download file with limit speed.

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -3,7 +3,10 @@ package ru.job4j.concurrent;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Paths;
 
 /**
  * Wget.
@@ -11,42 +14,35 @@ import java.net.URL;
  * Download file with given speed in bytes.
  * Need to specify arguments:
  * 1. full url to file with filename.
- * 2. speed in byte.
+ * 2. speed in byte/s.
  *
  * @author fourbarman (maks.java@yandex.ru).
  * @version %I%, %G%.
- * @since 12.12.2021.
+ * @since 14.12.2021.
  */
 public class Wget implements Runnable {
     private final String url;
-    private final int speed;
+    private final long speed;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, long speed) {
         this.url = url;
         this.speed = speed;
     }
 
     /**
-     * Calculates difference in current download time and given time.
+     * Validate args.
      *
-     * @param speed     Given speed.
-     * @param timeStart Current time before.
-     * @param timeEnd   Current time after.
-     * @return Difference in times.
+     * @param argsNum args length.
+     * @throws IllegalArgumentException if condition doesn't met.
      */
-    private long getTime(int speed, long timeStart, long timeEnd) {
-        return (1024 / speed) - (timeEnd - timeStart);
-    }
-
-    /**
-     * Return filename from url.
-     *
-     * @param url Url.
-     * @return Filename.
-     */
-    private String getFileName(String url) {
-        int index = url.lastIndexOf("/");
-        return url.substring(index + 1);
+    private static void validateArgs(int argsNum) throws IllegalArgumentException {
+        if (argsNum != 2) {
+            throw new IllegalArgumentException(
+                    "Usage: 2 arguments must be provided. Full url to file and speed in bytes/s!"
+                            + System.lineSeparator()
+                            + "Example: https://somesite/somefile.txt 1048576"
+            );
+        }
     }
 
     /**
@@ -54,32 +50,40 @@ public class Wget implements Runnable {
      */
     @Override
     public void run() {
-        String fileName = getFileName(url);
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+             FileOutputStream fileOutputStream = new FileOutputStream(Paths.get(new URI(url).getPath()).getFileName().toString())) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            long bytesWrited = 0;
+            long timeStart = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                long timeStart = System.currentTimeMillis();
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-                long timeEnd = System.currentTimeMillis();
-                long diff = this.getTime(speed, timeStart, timeEnd);
-                if (diff > 0) {
-                    Thread.sleep(diff);
+                bytesWrited += bytesRead;
+                if (bytesWrited >= speed) {
+                    long diffTime = System.currentTimeMillis() - timeStart;
+                    timeStart = System.currentTimeMillis();
+                    bytesWrited = 0;
+                    if (diffTime < 1000) {
+                        Thread.sleep(1000 - diffTime);
+                    }
                 }
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (URISyntaxException | IOException ur) {
+            ur.printStackTrace();
         } catch (InterruptedException ie) {
             Thread.currentThread().interrupt();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
+        long t1 = System.currentTimeMillis();
+        validateArgs(args.length);
         String url = args[0];
-        int speed = Integer.parseInt(args[1]);
+        long speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
         wget.start();
         wget.join();
+        long t2 = System.currentTimeMillis();
+        System.out.println("Total time: " + (t2 - t1));
     }
 }
