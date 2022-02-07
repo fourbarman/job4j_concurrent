92b1b95757b8b0877fa9dd422075e4a885933435
fourbarman <42202792+fourbarman@users.noreply.github.com>
2022-02-07 17:00:21 +0400

    Thread pool.

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,97 @@
+package ru.job4j.pool;
+
+import ru.job4j.simpleblockingqueue.SimpleBlockingQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * ThreadPool.
+ * Represents thread pool.
+ * Uses SimpleBlockingQueue for tasks (with limit by tasks) and LikedList to store worker threads.
+ * Initializes by current CPU Threads.
+ *
+ * @author fourbarman (maks.java@yandex.ru).
+ * @version %I%, %G%.
+ * @since 07.02.2022.
+ */
+public class ThreadPool {
+    private final List<Thread> threads = new LinkedList<>();
+    private SimpleBlockingQueue<Runnable> tasks = null;
+    private volatile boolean isStopped = false;
+    int size;
+
+    public ThreadPool(int maxTasks) {
+        this.tasks = new SimpleBlockingQueue<>(maxTasks);
+        this.size = Runtime.getRuntime().availableProcessors();
+        for (int i = 0; i < size; i++) {
+            Thread workerThread = new WorkerThread(tasks);
+            this.threads.add(workerThread);
+            workerThread.start();
+        }
+    }
+
+    /**
+     * Except runnable task and offer it to queue.
+     * If Pool is stopped throws IllegalStateException.
+     *
+     * @param job Runnable task.
+     * @throws InterruptedException Exception.
+     */
+    public void work(Runnable job) throws InterruptedException {
+        if (isStopped) {
+            throw new IllegalStateException("Pool is stopped");
+        }
+        this.tasks.offer(job);
+    }
+
+    /**
+     * Set isStopped flag and interrupt working threads.
+     */
+    public void shutdown() {
+        this.isStopped = true;
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+    }
+
+    /**
+     * Wait for threads to complete all tasks from queue.
+     */
+    public synchronized void waitUntilAllTasksFinished() {
+        while (!this.tasks.isEmpty()) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * WorkerThread.
+     * Represents working threads for runnable tasks.
+     * Used in thread pool.
+     * Takes task from queue and executes it's run() method.
+     * Does nothing in catch block if tread is already interrupted to keep pool alive.
+     */
+    public class WorkerThread extends Thread {
+        SimpleBlockingQueue<Runnable> taskQueue;
+
+        public WorkerThread(SimpleBlockingQueue<Runnable> queue) {
+            this.taskQueue = queue;
+        }
+
+        @Override
+        public void run() {
+            while (!isStopped) {
+                try {
+                    Runnable r = this.taskQueue.poll();
+                    r.run();
+                } catch (InterruptedException e) {
+                    //write to log
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/threads/pool/ThreadPoolTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.threads.pool;
+
+import org.junit.Test;
+import ru.job4j.pool.ThreadPool;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * ThreadPoolTest.
+ *
+ * @author fourbarman (maks.java@yandex.ru).
+ * @version %I%, %G%.
+ * @since 02.02.2022.
+ */
+public class ThreadPoolTest {
+    /**
+     * Test when queue has 10 tasks that increment one variable than success.
+     *
+     * @throws InterruptedException Exception.
+     */
+    @Test
+    public void test1() throws InterruptedException {
+        ThreadPool threadPool = new ThreadPool(10);
+        AtomicInteger result = new AtomicInteger(0);
+        for (int i = 0; i < 10; i++) {
+            threadPool.work(result::incrementAndGet);
+        }
+        threadPool.waitUntilAllTasksFinished();
+        threadPool.shutdown();
+        assertThat(result.get(), is(10));
+    }
+
+    /**
+     * Test when thread pool is stopped than throw IllegalStateException.
+     *
+     * @throws InterruptedException Exception.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void test2() throws InterruptedException {
+        ThreadPool threadPool = new ThreadPool(10);
+        AtomicInteger result = new AtomicInteger(0);
+        for (int i = 0; i < 10; i++) {
+            threadPool.work(result::incrementAndGet);
+        }
+        threadPool.shutdown();
+        threadPool.work(System.out::println);
+    }
+}
